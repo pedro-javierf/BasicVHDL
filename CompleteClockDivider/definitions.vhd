80ed390efaa7b2d977c782cbbd9ed138eefd4454
Pedro Javier <pedro-javierf@users.noreply.github.com>
2020-01-10 19:15:45 +0100

    Add files via upload

---
+++ CompleteClockDivider/definitions.vhd
@@ -0,0 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package definitions is
+    -- Creates clk_out clock with a period T_out=T_in*(half_count+1)*2 
+    -- for half_count greater than 0. T_in=1/100MHz=10ns
+    constant FREQ_1HZ   : unsigned := "10111110101111000001111111"; -- 1Hz   => 49999999
+    constant FREQ_C1    : unsigned := "1100001101001111";           -- 1KHz  =>    49999
+    constant FREQ_C2    : unsigned := "1001110000111";              -- 10KHz =>     4999
+    
+    
+    -- Control Constants
+
+    -- COMPLETE...
+
+    constant W_CONTROL  : integer := COMPLETE...;   -- Control vector width
+
+    -- Status Constants
+
+    -- COMPLETE...
+
+    constant W_STATUS   : integer := COMPLETE...;   -- Status vector width
+end package definitions;
